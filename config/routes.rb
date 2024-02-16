ce4fa37663ecdcc849e889f11e59c8dfda6a6cb9
nicola camp <nicola.camp89@yahoo.com>
2024-02-16 08:13:27 +0000

    added additional pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
 
   get "about",   to: "pages#about",   as: :about
   get "contact", to: "pages#contact", as: :contact
+  get "collections", to: "pages#collections", as: :collections
+  get "rings", to: "pages#rings", as: :rings
+  get "braceletes", to: "pages#braceletes", as: :braceletes
+  get "necklaces", to: "pages#necklaces", as: :necklaces
+  get "broochescufflinks", to: "pages#broochescufflinks", as: :broochescufflinks
+
 end
